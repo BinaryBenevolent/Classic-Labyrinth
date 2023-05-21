a80b5c2e22509dd3b15a1ab5dc73d11ee2a3d270
kevin <73090726+BinaryBenevolent@users.noreply.github.com>
2023-05-21 13:26:27 +0700

    Create Game Over Canvas
    Creating UI when the ball get into the holes

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayManager : MonoBehaviour
 {
+    [SerializeField] private UnityEvent OnGameOver = new UnityEvent();
+    [SerializeField] private UnityEvent OnPlayerWin = new UnityEvent();
+
     public void GameOver()
     {
-        Debug.Log("Game Over");
+        Time.timeScale = 0f;
+        OnGameOver.Invoke();
     }
 
     public void PlayerWin()
     {
         Debug.Log("Player Win");
+        Time.timeScale = 0f;
+        OnPlayerWin.Invoke();
     }
 }
